dc4c2c11bf0c00231cd9be9b740d19c102d9be15
debbiewong22 <debbie@ware.hk>
2022-12-07 23:38:26 +0800

    do till ending page 2

--- Assets/Scripts/CommonUI/EndVideoManager.cs
+++ Assets/Scripts/CommonUI/EndVideoManager.cs
@@ -1,6 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using DG.Tweening;
+
+public enum EndVideoStage
+{
+    None,
+    Page1_Playing,
+    Page1_FastIn,
+    Page1_EndWaiting,
+    Page2_Playing,
+    Page2_FastIn,
+    Page2_EndWaiting
+}
 
 public class EndVideoManager : MonoBehaviour
 {
@@ -42,6 +54,29 @@ public class EndVideoManager : MonoBehaviour
     public RectTransform page4_imgRect;
     public ConversationModeBossObject page4_bossObj;
 
+    [Header("Curr")]
+    public EndVideoStage currStage = EndVideoStage.None;
+
+    Vector2 textPosTarget_Up = new Vector2(0, 540f);
+    Vector2 textPosTarget_Down = new Vector2(0, -540f);
+    Vector2 textPosTarget_On = new Vector2(0, 0);
+
+    Vector2 imgPosTarget_On = new Vector2(0, 0);
+    Vector2 imgPosTarget_Off = new Vector2(970, 0);
+
+    float aniTime_Text_SlowIn = 5f;
+    float aniTime_Text_FastIn = 0.3f;
+    float aniTime_Text_Out = 0.5f;
+
+    float aniTime_Img_SlowIn = 2f;
+    float aniTime_Img_FastIn = 0.3f;
+    float aniTime_Img_Out = 0.5f;
+
+    IEnumerator page1_Coroutine_Play;
+    IEnumerator page1_Coroutine_FastIn;
+    IEnumerator page2_Coroutine_Play;
+    IEnumerator page2_Coroutine_FastIn;
+
     //for share in multiple scenes
     void Awake()
     {
@@ -57,13 +92,111 @@ public class EndVideoManager : MonoBehaviour
         }
     }
 
+    private void Start()
+    {
+        Setup();
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.B))
+        {
+            ResetAll();
+        }
+        if (Input.GetKeyDown(KeyCode.N))
+        {
+            Play();
+        }
+        if (Input.GetKeyDown(KeyCode.M))
+        {
+            if (currStage == EndVideoStage.Page1_Playing)
+            {
+                page1_Coroutine_FastIn = Page1_Ani_FaseIn();
+                StartCoroutine(page1_Coroutine_FastIn);
+            }
+            if (currStage == EndVideoStage.Page1_EndWaiting)
+            {
+                page2_Coroutine_Play = Page2_Ani_Play();
+                StartCoroutine(page2_Coroutine_Play);
+            }
+        }
+    }
+
+    public void Setup()
+    {
+        page1_Coroutine_Play = Page1_Ani_Play();
+        page1_Coroutine_FastIn = Page1_Ani_FaseIn();
+        page2_Coroutine_Play = Page2_Ani_Play();
+        ResetAll();
+    }
+
     public void Play()
     {
+        page1_Coroutine_Play = Page1_Ani_Play();
+        StartCoroutine(page1_Coroutine_Play);
+    }
 
+    IEnumerator Page1_Ani_Play()
+    {
+        currStage = EndVideoStage.Page1_Playing;
+        blackBkgRect.DOScale(new Vector3(1, 1, 1), 1f).SetEase(Ease.Linear);
+        yield return new WaitForSeconds(0.7f);
+        page1_textRect_TC.DOAnchorPos(textPosTarget_On, aniTime_Text_SlowIn).SetEase(Ease.Linear);
+        page1_imgRect.DOAnchorPos(imgPosTarget_On, aniTime_Img_SlowIn).SetEase(Ease.Linear);
+        yield return new WaitForSeconds(aniTime_Text_SlowIn);
+        currStage = EndVideoStage.Page1_EndWaiting;
+    }
+
+    IEnumerator Page1_Ani_FaseIn()
+    {
+        currStage = EndVideoStage.Page1_FastIn;
+        DOTween.Kill(blackBkgRect);
+        DOTween.Kill(page1_textRect_TC);
+        DOTween.Kill(page1_textRect_SC);
+        DOTween.Kill(page1_textRect_EN);
+        DOTween.Kill(page1_imgRect);
+        blackBkgRect.DOScale(new Vector3(1, 1, 1), aniTime_Img_FastIn).SetEase(Ease.Linear);
+        page1_textRect_TC.DOAnchorPos(textPosTarget_On, aniTime_Text_FastIn).SetEase(Ease.Linear);
+        page1_imgRect.DOAnchorPos(imgPosTarget_On, aniTime_Img_FastIn).SetEase(Ease.Linear);
+        yield return new WaitForSeconds(aniTime_Img_FastIn);
+        currStage = EndVideoStage.Page1_EndWaiting;
+    }
+
+    IEnumerator Page2_Ani_Play()
+    {
+        page1_textRect_TC.DOAnchorPos(textPosTarget_Up, aniTime_Text_Out).SetEase(Ease.Linear);
+        page1_imgRect.DOAnchorPos(imgPosTarget_Off, aniTime_Img_Out).SetEase(Ease.Linear);
+        yield return new WaitForSeconds(aniTime_Text_Out);
+        currStage = EndVideoStage.Page2_Playing;
+        page2_textRect_TC.DOAnchorPos(textPosTarget_On, aniTime_Text_SlowIn).SetEase(Ease.Linear);
+        page2_imgRect.DOAnchorPos(imgPosTarget_On, aniTime_Img_SlowIn).SetEase(Ease.Linear);
+        yield return new WaitForSeconds(aniTime_Text_SlowIn);
+        currStage = EndVideoStage.Page2_EndWaiting;
     }
 
     public void ResetAll()
     {
-
+        currStage = EndVideoStage.None;
+        StopCoroutine(page1_Coroutine_Play);
+        StopCoroutine(page1_Coroutine_FastIn);
+        StopCoroutine(page2_Coroutine_Play);
+        DOTween.Kill(blackBkgRect);
+        DOTween.Kill(page1_textRect_TC);
+        DOTween.Kill(page1_textRect_SC);
+        DOTween.Kill(page1_textRect_EN);
+        DOTween.Kill(page1_imgRect);
+        DOTween.Kill(page2_textRect_TC);
+        DOTween.Kill(page2_textRect_SC);
+        DOTween.Kill(page2_textRect_EN);
+        DOTween.Kill(page2_imgRect);
+        blackBkgRect.localScale = new Vector3(1, 0, 1);
+        page1_textRect_TC.anchoredPosition = textPosTarget_Down;
+        page1_textRect_SC.anchoredPosition = textPosTarget_Down;
+        page1_textRect_EN.anchoredPosition = textPosTarget_Down;
+        page1_imgRect.anchoredPosition = imgPosTarget_Off;
+        page2_textRect_TC.anchoredPosition = textPosTarget_Down;
+        page2_textRect_SC.anchoredPosition = textPosTarget_Down;
+        page2_textRect_EN.anchoredPosition = textPosTarget_Down;
+        page2_imgRect.anchoredPosition = imgPosTarget_Off;
     }
 }
