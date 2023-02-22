e92ae0e4788dbb6064e3df2ec9a6227126be398f
debbiewong22 <debbie@ware.hk>
2023-02-22 18:50:40 +0800

    update collection book boss card success collect use img but not material shader that affect fps

--- Assets/Scripts/CollectionBook/CollectionBookBossObject.cs
+++ Assets/Scripts/CollectionBook/CollectionBookBossObject.cs
@@ -15,12 +15,9 @@ public class CollectionBookBossObject : MonoBehaviour
     Vector3 originalScale;
 
     [Header("Card")]
+    public Image img_Glow;
     public Image img_Boss;
-    public UIEffect uiEffect_Boss;
-    public UIShadow uiShadow_Boss;
     public Image img_Lock;
-    public UIEffect uiEffect_Lock;
-    public UIShadow uiShadow_Lock;
     public CanvasGroup img_Gray;
 
     [Header("Text")]
@@ -52,15 +49,10 @@ public class CollectionBookBossObject : MonoBehaviour
         rect.localScale = originalScale;
         img_Lock.rectTransform.eulerAngles = new Vector3(0, 0, 0);
         img_Lock.color = new Color(1, 1, 1, 1);
-        uiShadow_Lock.effectColor = new Color(1, 1, 1, 0);
-        uiShadow_Lock.enabled = false;
-        uiEffect_Lock.enabled = false;
+        img_Glow.DOFade(0, 0);
         img_Lock.gameObject.SetActive(true);
         img_Boss.rectTransform.eulerAngles = new Vector3(0, 90, 0);
         img_Boss.color = new Color(1, 1, 1, 1);
-        uiShadow_Boss.effectColor = new Color(1, 1, 1, 0);
-        uiShadow_Boss.enabled = false;
-        uiEffect_Boss.enabled = false;
         img_Boss.gameObject.SetActive(false);
         img_Gray.DOFade(0, 0);
         ChangeLanguage(lang);
@@ -125,18 +117,15 @@ public class CollectionBookBossObject : MonoBehaviour
         {
             img_Gray.DOFade(0, 0);
             rect.DOScale(new Vector3(1.3f, 1.3f, 1.3f), 0.5f);
-            uiEffect_Lock.enabled = true;
-            uiShadow_Lock.enabled = true;
-            DOTween.To(() => uiShadow_Lock.effectColor, x => uiShadow_Lock.effectColor = x, new Color(1, 1, 1, 0.5f), 1f);
+            img_Glow.DOFade(1, 1);
             yield return new WaitForSeconds(0.8f);
             img_Lock.rectTransform.DOLocalRotate(new Vector3(0, 90, 0), 0.2f);
+            img_Glow.rectTransform.DOLocalRotate(new Vector3(0, 90, 0), 0.2f);
             yield return new WaitForSeconds(0.2f);
             img_Lock.gameObject.SetActive(false);
             img_Boss.gameObject.SetActive(true);
-            uiEffect_Boss.enabled = true;
-            uiShadow_Boss.enabled = true;
-            uiShadow_Boss.effectColor = new Color(1, 1, 1, 0.5f);
             img_Boss.rectTransform.DOLocalRotate(new Vector3(0, 0, 0), 0.2f);
+            img_Glow.rectTransform.DOLocalRotate(new Vector3(0, 0, 0), 0.2f);
             yield return new WaitForSeconds(0.8f);
             rect.DOScale(new Vector3(1f, 1f, 1f), 0.5f);
         }
@@ -179,15 +168,10 @@ public class CollectionBookBossObject : MonoBehaviour
         rect.localScale = originalScale;
         img_Lock.rectTransform.eulerAngles = new Vector3(0, 0, 0);
         img_Lock.color = new Color(1, 1, 1, 1);
-        uiShadow_Lock.effectColor = new Color(1, 1, 1, 0);
-        uiShadow_Lock.enabled = false;
-        uiEffect_Lock.enabled = false;
+        img_Glow.DOFade(0, 0);
         img_Lock.gameObject.SetActive(true);
         img_Boss.rectTransform.eulerAngles = new Vector3(0, 90, 0);
         img_Boss.color = new Color(1, 1, 1, 1);
-        uiShadow_Boss.effectColor = new Color(1, 1, 1, 0);
-        uiShadow_Boss.enabled = false;
-        uiEffect_Boss.enabled = false;
         img_Boss.gameObject.SetActive(false);
         img_Gray.DOFade(0, 0);
     }
